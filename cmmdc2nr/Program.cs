1919941013eb0654a52376ea0199871ec634155a
Liviu Tudor <liviutudor86@gmail.com>
2019-03-09 11:33:27 +0200

    added project

---
+++ cmmdc2nr/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cmmdc2nr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nr1, nr2, n, m;
+            Console.Write("Introduceti primul numar: ");
+            nr1 = int.Parse(Console.ReadLine());
+            Console.Write("Introduceti al doilea numar: ");
+            nr2 = int.Parse(Console.ReadLine());
+            n = nr1; m = nr2;
+            while (nr1 != nr2)
+                if (nr1 > nr2)
+                    nr1 -= nr2;
+                else
+                    nr2 -= nr1;
+            Console.WriteLine("c.m.m.d.c. ({0},{1}) = {2}", n, m, nr1);
+            Console.ReadLine();
+        }       
+    }
+}
