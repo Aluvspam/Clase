c32b15564657d19654aa66e341c5ee275ddb6801
andrei patrascu <andreipatrascu@outlook.com>
2019-03-09 11:22:47 +0200

    created Teme project

---
+++ Teme/TemeMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teme
+{
+    class TemeMain
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
