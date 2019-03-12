bb9bdd529101f311192aa8cf450a1c3dc4e7f28f
dinumarius21@yahoo.com <dinumarius21@yahoo.com>
2019-03-12 21:14:17 +0200

    add gril clase

---
+++ Clase/Girl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase
+{
+    public class Girl
+    {
+        private int age = 28;
+        public int getAge()
+        {
+        return 20;
+        }
+      }
+ }
+
