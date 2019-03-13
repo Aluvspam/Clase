d83a178d5d4a8caefa763a025b7bceef1312492c
Liviu Tudor <liviutudor86@gmail.com>
2019-03-13 19:21:47 +0200

    re-done chicken

--- Clase/Chicken.cs
+++ Clase/Chicken.cs
@@ -41,21 +41,19 @@ namespace Clase
             return;
         }
 
-        public int Eggs()
-        {
-            
+        public int LayingEggsCapacity()
+        {            
             int day = 1;
-            int week = day * 7;
             int layeggs = 1;
-            int egg = 2;
+            int eggprice = 2;
             int eggs = layeggs * day;
-            int eggs2 = egg * eggs;
+            int incomeforeggs = eggprice * eggs;
             
             if (health >= 0 && age <= 8)
             {
                 if (health >= 50)
                     return eggs * 2;
-                return eggs;
+                return eggs + incomeforeggs;
             }
             else
                 return 0;          
