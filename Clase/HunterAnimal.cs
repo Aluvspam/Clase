fed86478a5f96912d0dde107c08c682fcf50f29a
andrei patrascu <andreipatrascu@outlook.com>
2019-03-20 19:04:10 +0200

    changes (wind of)

--- Clase/HunterAnimal.cs
+++ Clase/HunterAnimal.cs
@@ -8,11 +8,23 @@ namespace Clase
 {
     public abstract class HunterAnimal : Animal
     {
-        public int PreyMaxWeight { get; set; }
+        protected float PreyMaxWeight { get; set; }
 
         public virtual void EatAnimal()
         {
+            if (this.owner != null)
+            {
+                var pets = this.owner.Pets;
 
+                foreach (var pet in pets)
+                {
+                    if (pet.weight <= PreyMaxWeight)
+                    {
+                        Console.WriteLine("Eat " + pet.Name);
+                        pets.Remove(pet);
+                    }
+                }
+            }
         }
     }
 }
