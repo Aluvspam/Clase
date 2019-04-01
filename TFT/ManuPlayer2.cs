df1dc8e010dfe4ef27b14b2db03990bbe5cf8378
andrei_manu@outlook.com <andrei_manu@outlook.com>
2019-04-01 18:30:03 +0300

    Manu players

---
+++ TFT/ManuPlayer2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFT
+{
+    class ManuPlayer2 : IPlayer, IScore
+    {
+        public int Score
+        {
+            get
+            {
+                return 0;
+            }
+        }
+        Random rnd;
+        public ManuPlayer2()
+        {
+            rnd = new Random();
+
+        }
+        public Moves FirstMove()
+        {
+
+            return Moves.C;
+        }
+
+        public Moves MyMove(Moves myLastMove, Moves othersLastMove)
+        {
+
+            if (othersLastMove == Moves.D)
+            {
+                return Moves.D;
+            }
+            if (othersLastMove == Moves.C)
+            {
+                return Moves.D;
+            }
+            if (othersLastMove == Moves.B)
+            {
+                return Moves.D;
+            }
+            return Moves.D;
+        }
+
+        private Moves RandomMove()
+        {
+            return (rnd.Next(2) == 0) ? Moves.C : Moves.D;
+        }
+    }
+}
