c4216ecbdd143eff0fee0d2ec8ffebde8dccae73
teo.simion teo.simion <teodoraas19@gmail.com>
2019-04-01 19:10:07 +0300

    Player1

---
+++ ClaseTests/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase
+{
+    public class Teacher : Person
+    {
+        public string Experience;
+        public Teacher() : base(Gender.male, "Femele", null, "", null, 0, null, false)
+        {
+            Experience = "multa";
+        }
+    }
+}
