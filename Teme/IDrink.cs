181bb1686da2627dcada6dcf3853283986d494fa
Alex Manta <alexandru.manta@hotmail.com>
2019-04-23 15:01:00 +0300

    Tema IDrink incompleta

---
+++ Teme/IDrink.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curs15.Decorator.Tema
+{
+    public interface IDrink
+    {
+        int GetCost();
+        int GetQuantity();
+
+    }
+    public interface Ijuice : IDrink
+    {
+        Ijuice Component
+        {
+            get; set;
+        }
+
+    }
+    public interface ICocktail : IDrink, Ijuice
+    {
+        int TotalCost(int quantity, int cost);
+    }
+
+    public class Gin : IDrink
+    {
+        private int cost = 2;
+        private int quantity = 25;
+
+        public int GetCost()
+        {
+            return cost;
+        }
+        public int GetQuantity()
+        {
+            return quantity;
+        }
+    }
+
+    public class Vodka : IDrink
+    {
+        private int cost = 3;
+        private int quantity = 25;
+
+
+        public int GetCost()
+        {
+            return cost;
+        }
+        public int GetQuantity()
+        {
+            return quantity;
+        }
+    }
+
+    public class JB : IDrink
+    {
+        private int cost = 2;
+        private int quantity = 25;
+
+
+        public int GetCost()
+        {
+            return cost;
+        }
+        public int GetQuantity()
+        {
+            return quantity;
+        }
+    }
+
+    public class Rom : IDrink
+    {
+        private int cost = 3;
+        private int quantity = 25;
+
+        public int GetCost()
+        {
+            return cost;
+        }
+        public int GetQuantity()
+        {
+            return quantity;
+        }
+    }
+
+    public class Tonic : Ijuice
+    {
+        private int cost = 3;
+        private int quantity = 25;
+
+        public Ijuice Component { get; set; }
+
+        public int GetCost()
+        {
+            if (Component != null)
+            {
+                return cost + Component.GetCost();
+            }
+            else
+            {
+                return cost;
+            }
+        }
+        public int GetQuantity()
+        {
+            return quantity;
+        }
+    }
+    public class Cola : Ijuice
+    {
+        private int cost = 3;
+        private int quantity = 25;
+
+        public Ijuice Component { get; set; }
+
+        public int GetCost()
+        {
+            if (Component != null)
+            {
+                return cost + Component.GetCost();
+            }
+            else
+            {
+                return cost;
+            }
+        }
+        public int GetQuantity()
+        {
+            return quantity;
+        }
+    }
+
+    public class Orange : Ijuice
+    {
+        private int cost = 3;
+        private int quantity = 25;
+
+        public Ijuice Component { get; set; }
+
+        public int GetCost()
+        {
+            if (Component != null)
+            {
+                return cost + Component.GetCost();
+            }
+            else
+            {
+                return cost;
+            }
+        }
+        public int GetQuantity()
+        {
+            return quantity;
+        }
+    }
+
+    public class Cranberry : Ijuice
+    {
+        private int cost = 3;
+        private int quantity = 25;
+
+        public Ijuice Component { get; set; }
+
+        public int GetCost()
+        {
+            if (Component != null)
+            {
+                return cost + Component.GetCost();
+            }
+            else
+            {
+                return cost;
+            }
+        }
+        public int GetQuantity()
+        {
+            return quantity;
+        }
+    }
+}
+
+
+
---
+++ Teme/TemaRunExamples.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curs15.Decorator.Tema
+{
+    public class TemaRunExamples
+    {
+        public static void Run()
+        {
+            IDrink drink = new Drink();
+
+            Ijuice orange = new Orange();
+            orange.Component = gin;
+            
+        }
+    }
+}
