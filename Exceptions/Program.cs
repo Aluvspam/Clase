a53ff6a4fd268a65b5b5f7e1607b7ed5d93c6249
andrei patrascu <andreipatrascu@outlook.com>
2019-06-10 20:42:40 +0300

    logs added for Exceptions project

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -4,11 +4,13 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace Exceptions
 {
     class Program
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
             int nr = 0;
@@ -19,36 +21,52 @@ namespace Exceptions
                 {
                     nr = Reading();
 
-                    Console.WriteLine("numarul este " + nr);
+                    logger.Info("numarul este " + nr);
                 }
                 catch (MyException me)
                 {
-                    Console.WriteLine("myExc message is " + me.MyField);
+                    logger.Error("myExc message is " + me.MyField);
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine("nu inteleg numarul");
-                    Console.WriteLine("exception message is " + e.Message);
+                    logger.Error("exception message is " + e.Message);
                 }
                 finally
                 {
                     Console.WriteLine("asta se executa oricum");
+                    logger.Trace("asta se executa oricum");
                 }
             } while (nr == 0);
             Console.ReadLine();
-            StreamReader se = new StreamReader("path.txt");
+            StreamReader se = null;
             try
             {
+                se = new StreamReader("path.txt");
+                logger.Trace("am deschis StreamReader catre path.txt");
                 var v = int.Parse(se.ReadLine());
                 var b = 3 / v;
+            }
+            catch (FileNotFoundException)
+            {
+                logger.Error("oopsie, eroare cand am vrut sa deschid StreamReader(\"path.txt\")");
             }
             catch (Exception)
             {
                 Console.WriteLine("oopsie, eroare");
+                logger.Error("oopsie, eroare");
             }
             finally
             {
-                se.Close();
+                if (se != null)
+                {
+                    se.Close();
+                    logger.Trace("am inchis StreamReader catre path.txt");
+                }
+                else
+                {
+                    logger.Warn("null StreamReader");
+                }
             }
         }
         static int Reading()
