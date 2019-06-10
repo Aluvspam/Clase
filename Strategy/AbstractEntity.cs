063e8917062434b6ad0cb7db67fd1e66c63e0a65
andrei_manu@outlook.com <andrei_manu@outlook.com>
2019-06-10 21:04:47 +0300

    loog

--- Strategy/AbstractEntity.cs
+++ Strategy/AbstractEntity.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace Solid_Examples.AndreiR
 {
     public abstract class AbstractEntity
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
         protected string name;
         protected IMoveStrategy moveStrategy;
         public virtual void Move()
         {
             Console.Write("I'm a {0}", name);
             moveStrategy.Move();
+        }
+        public AbstractEntity()
+        {
+
+
+                    
         }
     }
 }
