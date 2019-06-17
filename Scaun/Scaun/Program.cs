d128e82b015c5b83553b5e410c7e7ce838f03c88
Liviu Tudor <liviutudor86@gmail.com>
2019-06-17 19:34:13 +0300

    Problema Interviu

--- Scaun/Scaun/Program.cs
+++ Scaun/Scaun/Program.cs
@@ -12,6 +12,27 @@ namespace Scaun
         {
             Scaun S = new Scaun();
             Scaun S2 = new Scaun();
+
+            var V = new char [12] {'1', '3', '6', '7', 'A', 'b', 'd', 'E', 'F', 'g', '8', 'W'};
+            var result = new List<char>();
+
+            result = ProblemaInterviu(V);
         }
+
+        public static List<char> ProblemaInterviu(char[] V)
+        {
+            var target = new List<char>();
+
+            foreach (var item in V)
+            {
+                if (item >= 'A' && item <= 'Z')
+                {
+                    target.Add(item);
+                }
+            }
+
+            return target;
+        } 
+
     }
 }
