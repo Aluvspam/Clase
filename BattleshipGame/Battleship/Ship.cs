fdfe9873f120451f8b81a792236203e15916f3b3
Liviu Tudor <liviutudor86@gmail.com>
2019-07-10 21:18:41 +0300

    Battleship game

---
+++ BattleshipGame/Battleship/Ship.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Ship
+    {
+        public Point Start;
+        public Point Finish;
+        public int Spaces;
+
+        public Ship(Point Start, Point Finish)
+        {
+            this.Start = Start;
+            this.Finish = Finish;
+            if (Start.x == Finish.x)
+            {
+                Spaces = Math.Abs(Start.y - Finish.y);
+            }
+
+            if (Start.y == Finish.y)
+            {
+                Spaces = Math.Abs(Start.x - Finish.x);
+            }
+        }
+    }
+}
