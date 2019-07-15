47a0af4c98505b7a7b6c1c93dedd56e7b69663ef
andrei_manu@outlook.com <andrei_manu@outlook.com>
2019-07-15 19:56:37 +0300

    modif

--- BattleshipGame/Battleship/Player.cs
+++ BattleshipGame/Battleship/Player.cs
@@ -16,74 +16,91 @@ namespace Battleship
         {
             var p1 = new Point(1, 1);
             var p2 = new Point(1, 2);
-
             var minesweeper1 = new Ship(p1,p2);
 
             p1.x = 7;
             p1.y = 1;
-
             p2.x = 7;
             p2.y = 2;
-
             var minesweeper2 = new Ship(p1, p2);
 
             p1.x = 4;
             p1.y = 5;
-
             p2.x = 6;
             p2.y = 5;
-
             var battleship = new Ship(p1, p2);
             
             p1.x = 9;
             p1.y = 5;
-
             p2.x = 9;
             p2.y = 8;
-
             var destroyer = new Ship(p1, p2);
 
             Fleet = new List<Ship>() { minesweeper1, minesweeper2, battleship, destroyer };
 
-            map = new Dictionary<Point, bool>();
+            //map = new Dictionary<Point, bool>();
 
-            foreach (var ship in Fleet)
-            {
-                if(ship.Start.x == ship.Finish.x)
-                {
-                    for (int i = ship.Start.y; i <= ship.Finish.y; i++)
-                    {
-                        map.Add(new Point(ship.Start.x, i), true);
-                    }
-                }
+            //foreach (var ship in Fleet)
+            //{
+            //    if(ship.Start.x == ship.Finish.x)
+            //    {
+            //        for (int i = ship.Start.y; i <= ship.Finish.y; i++)
+            //        {
+            //            map.Add(new Point(ship.Start.x, i), true);
+            //        }
+            //    }
 
-                if (ship.Start.y == ship.Finish.y)
-                {
-                    for (int i = ship.Start.x; i <= ship.Finish.x; i++)
-                    {
-                        map.Add(new Point(i, ship.Start.y), true);
-                    }
-                }
-            }                    
-                                
+            //    if (ship.Start.y == ship.Finish.y)
+            //    {
+            //        for (int i = ship.Start.x; i <= ship.Finish.x; i++)
+            //        {
+            //            map.Add(new Point(i, ship.Start.y), true);
+            //        }
+            //    }
+            //}                    
         }
 
         public string Attack(Point attack)
         {
             string result = null;
-
-            if (map.ContainsKey(attack))
+            bool ok = false;
+            foreach (var ship in Fleet)
             {
-                map[attack] = false; 
-                if()
+                if (ship.Spaces.ContainsKey(attack))
                 {
-
+                    ok = true;
+                    ship.Spaces.Remove(attack);
+                    if(ship.Spaces.Count() > 0)
+                    {
+                        result = "hit";
+                    }
+                    else
+                    {
+                        Fleet.Remove(ship);
+                        if(Fleet.Count() > 0)
+                        {
+                            result = "sink";
+                        }
+                        else
+                        {
+                            result = "surrender";
+                        }
+                    }
                 }
             }
-            else
+            if(ok == false)
             {
                 result = "miss";
             }
+
+            //if (map.ContainsKey(attack))
+            //{
+            //    map[attack] = false; 
+            //}
+            //else
+            //{
+            //    result = "miss";
+            //}
 
             return result;
         }
--- BattleshipGame/Battleship/Ship.cs
+++ BattleshipGame/Battleship/Ship.cs
@@ -10,20 +10,28 @@ namespace Battleship
     {
         public Point Start;
         public Point Finish;
-        public int Spaces;
+        public Dictionary<Point, bool> Spaces;
 
         public Ship(Point Start, Point Finish)
         {
+
             this.Start = Start;
             this.Finish = Finish;
+            var s = new Dictionary<Point, bool>();
             if (Start.x == Finish.x)
             {
-                Spaces = Math.Abs(Start.y - Finish.y);
+                for(int i = Start.y; i <= Finish.y; i++)
+                {
+                    s.Add(new Point(Start.x, i), true);
+                }
             }
 
             if (Start.y == Finish.y)
             {
-                Spaces = Math.Abs(Start.x - Finish.x);
+                for (int i = Start.x; i <= Finish.x; i++)
+                {
+                    s.Add(new Point(i, Start.y), true);
+                }
             }
         }
     }
