49105fce5bab9fe8fdf9855b5e7f9f6f0053af26
teo.simion teo.simion <teodoraas19@gmail.com>
2019-07-21 11:39:55 +0300

    corectare ctor Ship

--- BattleshipGame/Battleship/Ship.cs
+++ BattleshipGame/Battleship/Ship.cs
@@ -18,8 +18,16 @@ namespace Battleship
             this.Start = Start;
             this.Finish = Finish;
             Spaces = new Dictionary<Point, bool>();
+
             if (Start.row == Finish.row)
             {
+                if (Start.column > Finish.column)
+                {
+                    Point temp = new Point(Start.row, Start.column);
+                    Start = Finish;
+                    Finish = temp;
+                }
+
                 for(int i = Start.column; i <= Finish.column; i++)
                 {
                     Spaces.Add(new Point(Start.row, i), true);
@@ -28,6 +36,13 @@ namespace Battleship
 
             if (Start.column == Finish.column)
             {
+                if (Start.row > Finish.row)
+                {
+                    Point temp = new Point(Start.row, Start.column);
+                    Start = Finish;
+                    Finish = temp;
+                }
+
                 for (int i = Start.row; i <= Finish.row; i++)
                 {
                     Spaces.Add(new Point(i, Start.column), true);
